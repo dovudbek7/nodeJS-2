85a4b67bbf48003fdfc2e4e85fd9c505bbb6fdea
dovudbek7 <dovudbekm99@gmail.com>
2026-06-17 17:21:20 +0500

    req and res routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import { env } from '../env.ts'
 import { app } from './server.ts'
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000...")
+app.listen(env.PORT, () => {
+  console.log(`Server running on port ${env.PORT}...`)
 })
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,13 @@ app.get('/health', (req, res) => {
   res.send('<button>Click</button>')
 })
 
+// app.post('/cake', (req, res) => {
+//   res.send('ok')
+// })
+app.post('/cake/:name/:id', (req, res) => {
+  res.json(req.params)
+})
+
 export { app }
 
 export default app
