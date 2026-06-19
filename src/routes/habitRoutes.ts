b1da73dad7751ab51f3c4041cc918d7bfe504764
dovudbek7 <dovudbekm99@gmail.com>
2026-06-19 16:53:05 +0500

    parametrs & query  validators

--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -1,28 +1,36 @@
 import { Router } from 'express'
 import { ro } from 'zod/v4/locales'
-import { validateBody } from '../middleware/validations.ts'
+import { validateBody, validateParams } from '../middleware/validations.ts'
 import z, { string } from 'zod'
 
 const router = Router()
 
-const habitSchema = z.object({
+const createHabitSchema = z.object({
   name: string(),
 })
 
+const completeHabitSchema = z.object({
+  id: z.string(),
+})
 router.get('/', (req, res) => {
   res.json({ message: 'habits' })
 })
 router.get('/:id', (req, res) => {
   res.json({ message: 'get one habit' })
 })
-router.post('/', validateBody(habitSchema), (req, res) => {
+router.post('/', validateBody(createHabitSchema), (req, res) => {
   res.json({ message: 'created habit' }).status(201)
 })
 router.delete('/:id', (req, res) => {
   res.json({ message: 'Deleted habit' })
 })
-router.post('/:id/complete', (req, res) => {
-  res.json({ message: 'Deleted habit' })
-})
+router.post(
+  '/:id/complete',
+  validateParams(completeHabitSchema),
+  validateBody(createHabitSchema),
+  (req, res) => {
+    res.json({ message: 'Deleted habit' })
+  },
+)
 
 export default router
