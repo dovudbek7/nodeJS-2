49543aba022df4dbdd2ddb123a98728792075752
dovudbek7 <dovudbekm99@gmail.com>
2026-06-19 16:30:22 +0500

    validation middleware

--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express'
 import { ro } from 'zod/v4/locales'
+import { validateBody } from '../middleware/validations.ts'
+import z, { string } from 'zod'
 
 const router = Router()
+
+const habitSchema = z.object({
+  name: string(),
+})
 
 router.get('/', (req, res) => {
   res.json({ message: 'habits' })
@@ -9,7 +15,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   res.json({ message: 'get one habit' })
 })
-router.post('/', (req, res) => {
+router.post('/', validateBody(habitSchema), (req, res) => {
   res.json({ message: 'created habit' }).status(201)
 })
 router.delete('/:id', (req, res) => {
