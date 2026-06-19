29e43779c3e80163e320bac609758cb26a9a8371
dovudbek7 <dovudbekm99@gmail.com>
2026-06-19 16:01:02 +0500

    validation middleware

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,18 @@ import express from 'express'
 import userRoutes from './routes/userRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
 import authRoutes from './routes/authRoutes.ts'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import cors from 'cors'
 
+import { isTest } from '../env.ts'
 const app = express()
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev', { skip: () => isTest() }))
 
 app.get('/health', (req, res) => {
   // res.json({ message: 'Hello' }).status(200)
@@ -13,6 +23,7 @@ app.get('/health', (req, res) => {
 // app.post('/cake', (req, res) => {
 //   res.send('ok')
 // })
+
 app.post('/cake/:name/:id', (req, res) => {
   res.json(req.params)
 })
